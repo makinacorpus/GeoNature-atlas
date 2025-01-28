63caa4bc3d467187fc619c539fd624d164979a74
Joaquim Nallar <jna@makina-corpus.com>
2025-01-28 14:22:19 +0100

    fix: parameter name to display tab

--- atlas/configuration/config_schema.py
+++ atlas/configuration/config_schema.py
@@ -142,9 +142,9 @@ class AtlasConfig(Schema):
     AFFICHAGE_GRAPH_ALTITUDES = fields.Boolean(load_default=True)
     AFFICHAGE_GRAPH_PHENOLOGIE = fields.Boolean(load_default=False)
     AFFICHAGE_GRAPH_PROVENANCE_DONNEE = fields.Boolean(load_default=False)
-    AFFICHAGE_GRAPH_AREA_GENERAL_PRESENTATION = fields.Boolean(load_default=False)
-    AFFICHAGE_GRAPH_AREA_OBS_ESPECES = fields.Boolean(load_default=False)
-    AFFICHAGE_GRAPH_AREA_PROVENANCE_DONNEE = fields.Boolean(load_default=False)
+    AFFICHAGE_TAB_AREA_GENERAL_PRESENTATION = fields.Boolean(load_default=False)
+    AFFICHAGE_TAB_AREA_OBS_ESPECES = fields.Boolean(load_default=False)
+    AFFICHAGE_TAB_AREA_PROVENANCE_DONNEE = fields.Boolean(load_default=False)
     TYPE_TERRITOIRE_SPECIES = fields.List(fields.String(), load_default=["COM"])
     COLOR_STACKED_BAR_CHARTS = fields.List(
         fields.String(), load_default=["#E1CE7A", "#FBFFB9", "#FDD692"]
